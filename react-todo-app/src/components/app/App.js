0784f58b6707a0235173e6230cbf5aa842000656
Martin Bobcik <martin.bobcik@solarwinds.com>
2021-09-16 15:38:35 +0200

    R: folder structure for components

---
+++ react-todo-app/src/components/app/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello World! There will be todo list soon.
+      </div>
+    );
+  }
+}
+  
+export default App;
+  
